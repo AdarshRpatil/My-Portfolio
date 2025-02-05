616d2a5f460567d2a5085d63fdbc037e1e1e6127
AdarshRpatil <adarshrpatil02@gmail.com>
2025-02-05 23:29:41 +0530

    added resume

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
-// import CV from '';
+import CV from '../../assets/Adarsh_resume.pdf';
 
 const CTA = () => {
   return (
     <div className="cta">
-      {/* <a href="" download className="btn">
-        Download CV
-      </a> */}
-      <a className="btn">
+      <a href={CV} download className="btn">
         Download CV
       </a>
       <a href="#contact" className="btn btn-primary">
